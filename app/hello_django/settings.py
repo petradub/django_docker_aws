1f4b7bff796d6fc8ee34b102e7f4402f94429afb
petra <petra@petras-MBP.attlocal.net>
2023-09-15 21:34:38 -0400

    first commit

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for productionß
+# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['ec2-52-206-105-145.compute-1.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ß
+
 # Application definition
 
 INSTALLED_APPS = [
